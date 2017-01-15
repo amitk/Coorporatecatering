2f6bc275a07e4a222609e6813509bc336eb9152e
amitk <amit@joshsoftware.com>
2017-01-15 17:35:02 +0530

    added regex for email

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
-	validates :name,presence: {case_sensivity: false}
-	validates :email,presence: true
+	validates :name, presence: {case_sensivity: false}
+	validates :email, presence: true, format: {with: /[a-zA-z]\w*@\w*\.[a-z]*/, message: "Invalid email"} 
 
 	has_many :employees, dependent: :destroy
 	has_and_belongs_to_many :day_menu
